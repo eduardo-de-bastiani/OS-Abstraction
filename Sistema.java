d9db177d8df7fab7ccc668f3fd8cb696440c80d6
Eduardo De Bastiani <eduardocolladebastiani@gmail.com>
2025-03-16 11:30:57 -0300

    adding syscall read operation

--- Sistema.java
+++ Sistema.java
@@ -399,13 +399,16 @@ public class Sistema {
 	// ----------------------
 	public class SysCallHandling {
 		private HW hw; // referencia ao hw se tiver que setar algo
+		private Scanner in;
 
 		public SysCallHandling(HW _hw) {
 			hw = _hw;
+			in = new Scanner(System.in);
 		}
 
 		public void stop() { // chamada de sistema indicando final de programa
 							 // nesta versao cpu simplesmente pára
+							// vamos manter o print, pois o boolean cpuStop já encerra o programa
 			System.out.println("                                               SYSCALL STOP");
 		}
 
@@ -415,10 +418,15 @@ public class Sistema {
 			System.out.println("SYSCALL pars:  " + hw.cpu.reg[8] + " / " + hw.cpu.reg[9]);
 
 			if  (hw.cpu.reg[8]==1){
-				  // leitura ...
+				// leitura - le a entrada do teclado do usuario e guarda em reg[9]
+				System.out.println("IN: Leitura do teclado (apenas valores inteiros): ");
+				int input = in.nextInt();
+
+				// armazena a entrada no reg[9]
+				hw.mem.pos[hw.cpu.reg[9]].p = input;
 
 			} else if (hw.cpu.reg[8]==2){
-				  // escrita - escreve o conteuodo da memoria na posicao dada em reg[9]
+				  // escrita - escreve o conteudo da memoria na posicao dada em reg[9]
 				  System.out.println("OUT:   "+ hw.mem.pos[hw.cpu.reg[9]].p);
 			} else {System.out.println("  PARAMETRO INVALIDO"); }		
 		}
