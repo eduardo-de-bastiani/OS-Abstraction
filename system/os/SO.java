fee8c1dce7b88e38ebd347bda173de7ae98c3257
Matheus Chong <matheus.chong@gmail.com>
2025-04-26 11:39:46 -0300

    Ajusta parametros recebidos

--- system/os/SO.java
+++ system/os/SO.java
@@ -1,19 +1,22 @@
 package system.os;
 
 import system.hardware.HW;
+import system.software.ProcessManager;
 import system.utils.Utilities;
 
 public class SO {
     public InterruptHandling ih;
     public SysCallHandling sc;
     public Utilities utils;
-    public MemoryManager mm;
+    public MemoryManager mm; 
+    public ProcessManager pm;
 
-    public SO(HW hw) {
+    public SO(HW hw, int tamMem, int pageSize) {
         ih = new InterruptHandling(hw); // rotinas de tratamento de int
         sc = new SysCallHandling(hw); // chamadas de sistema
         hw.cpu.setAddressOfHandlers(ih, sc);
-        mm = new MemoryManager(16, hw);
+        mm = new MemoryManager(tamMem, pageSize);
+        pm = new ProcessManager(mm);
         utils = new Utilities(hw, mm); // utilitários do sistema
 
     }
