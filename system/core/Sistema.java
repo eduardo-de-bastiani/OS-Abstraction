e9a7a1d6f424774d45401ad862a111efe028df3e
Matheus Chong <matheus.chong@gmail.com>
2025-04-26 11:38:06 -0300

    Ajusta instancias de classes

--- system/core/Sistema.java
+++ system/core/Sistema.java
@@ -1,27 +1,20 @@
 package system.core;
 
 import system.hardware.HW;
-import system.os.MemoryManager;
 import system.os.SO;
 import system.software.Commands;
-import system.software.ProcessManager;
 import system.software.Programs;
 
 public class Sistema {
-    public int pageSize = 16;
     public HW hw;
     public SO so;
     public Programs progs;
-    public MemoryManager mm;
-    public ProcessManager pm;
 
-    public Sistema(int tamMem) {
+    public Sistema(int tamMem, int pageSize) {
         hw = new HW(tamMem); // memoria do HW tem tamMem palavras
-        so = new SO(hw);
+        so = new SO(hw, tamMem, pageSize);
         hw.cpu.setUtilities(so.utils); // permite cpu fazer dump de memoria ao avancar
         progs = new Programs();
-        mm = new MemoryManager(pageSize, hw); // gerenciador de memoria
-        pm = new ProcessManager(this); // gerenciador de processos
     }
 
     public void run() {
