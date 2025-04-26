41c9c3db7a771088bddbee8420d74bc5304df18a
Matheus Chong <matheus.chong@gmail.com>
2025-04-26 11:37:49 -0300

    Passa tamanho da pagina por referencia

--- system/core/Main.java
+++ system/core/Main.java
@@ -2,7 +2,7 @@ package system.core;
 
 public class Main {
     public static void main(String[] args) {
-        Sistema s = new Sistema(1024);
+        Sistema s = new Sistema(1024, 16);
         s.run();
     }
 }
