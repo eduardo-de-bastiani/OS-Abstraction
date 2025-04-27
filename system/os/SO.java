5ff3969a0ffe73bc9e545fdbd0cafeccea1406f6
Matheus Chong <matheus.chong@gmail.com>
2025-04-26 21:58:51 -0300

    Ajusta passagens de parametros entre classes

--- system/os/SO.java
+++ system/os/SO.java
@@ -2,6 +2,7 @@ package system.os;
 
 import system.hardware.HW;
 import system.software.ProcessManager;
+import system.software.Scalonator;
 import system.utils.Utilities;
 
 public class SO {
@@ -10,13 +11,16 @@ public class SO {
     public Utilities utils;
     public MemoryManager mm; 
     public ProcessManager pm;
+    public Scalonator sca;
 
     public SO(HW hw, int tamMem, int pageSize) {
         ih = new InterruptHandling(hw); // rotinas de tratamento de int
         sc = new SysCallHandling(hw); // chamadas de sistema
         hw.cpu.setAddressOfHandlers(ih, sc);
-        mm = new MemoryManager(tamMem, pageSize, hw.mem);
-        pm = new ProcessManager(mm);
+        sca = new Scalonator(); // inicializa o escalonador
+        pm = new ProcessManager();
+        mm = new MemoryManager(tamMem, pageSize, hw.mem, pm);
+        pm.memoryManager = mm; // gerenciador de processos tem acesso ao mm
         utils = new Utilities(hw, mm); // utilitários do sistema
 
     }
